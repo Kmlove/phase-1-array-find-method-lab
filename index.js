5090cf2609941641b38c3608d37cc216e3755b2c
Kim Love <kimberlymlove15@gmail.com>
2023-08-05 16:25:23 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(arrayOfObjects){
+    //1. Get the result value out of each object
+    let winningObj = arrayOfObjects.find((obj) => obj.result === "W")
+    
+    if(winningObj){
+        return winningObj.year
+    } else{
+        return undefined
+    }
+   
+}
